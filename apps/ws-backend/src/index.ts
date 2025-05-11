efa9b343987ceb3a99a9418f898656df457eedf9
gargsuryansh <gargsuryansh0808@gmail.com>
2025-05-12 01:44:26 +0530

    http-backend-completed

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,15 @@
+import { WebSocket, WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from '@repo/backend-common/config';
+import { prismaClient } from "@repo/db/client";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+
+wss.on('connection', function connection(ws, request) {
+  
+ 
+
+
+});
+
